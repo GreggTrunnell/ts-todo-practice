88caae90ac5bfc286e7556e3fe89da332707b1d1
Gregg Trunnell <gggtrunnell@gmail.com>
2025-08-15 17:03:53 -0500

    working with newTodo type

--- ts-todo-practice/src/App.tsx
+++ ts-todo-practice/src/App.tsx
@@ -4,15 +4,25 @@ import './App.css'
 function App() {
 
   type Todo = {
-    task: string,
+    todo: string,
     urgency: number,
     date: number,
     completed: false
   }
 
+  type newTodo ={
+    newTodo: string,
+    newUrgency: number,
+    newDate: number,
+  }
+  
   const [todo, setTodo] = useState<Todo[]>([])
   const [count, setCount] = useState<number>(0);
 
+  const addTodo = (newItem: string, newUrgency: number, newDate: number ) => {
+    console.log("addTodo hit");
+ 
+  }
 
   return (
     <div>
@@ -23,6 +33,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
+        <div>
+        <button onClick={addTodo}>addtodo</button>
+        </div>
     
     </div>
   )
